af03bb4e4e1d879c0b86df1a0e6acc96d4f12860
berkaygurler1 <114694053+berkaygurler1@users.noreply.github.com>
2022-10-30 22:49:28 +0300

    Initial commit

---
+++ todo.js
@@ -0,0 +1,54 @@
+// close butonunu oluşturalım
+var myNodelist=document.getElementsByTagName("LI");
+var i;
+for(i=0;i<myNodelist.length;i++){
+    var span=document.createElement("SPAN");
+    var txt=document.createTextNode("\u00D7");
+    span.className='close';
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+// close butonuna tıklayınca satırı gizlesin
+var close=document.getElementsByClassName('close');
+var i;
+for(i=0;i<close.length;i++){
+    close[i].onclick=function(){
+        var div=this.parentElement;
+        div.style.display="none";
+    }
+}
+// Satırlara tıklayınca üstünü çizmesi
+var list=document.querySelector('ul');
+list.addEventListener('click',function(ev){
+    if(ev.target.tagName === 'LI'){
+        ev.target.classList.toggle('checked');
+    }
+},false);
+
+// Listeye yapılacak ekleyelim ve Ekle butonunu aktifleştirelim
+var newElement;
+function newElement(){
+var li=document.createElement("li");
+var inputValue=document.getElementById("myInput").value;
+var t=document.createTextNode(inputValue);
+li.appendChild(t);
+if(inputValue === ''){
+    alert("Sakin ol dostum!");
+}else{
+    document.getElementById("myUl").appendChild(li);
+}
+document.getElementById("myInput").value="";
+var span=document.createElement("SPAN");
+var txt=document.createTextNode("\u00D7");
+span.className="close";
+span.appendChild(txt);
+li.appendChild(span);
+for(i=0;i<close.lenght;i++){
+    close[i].onclick=function(){
+        var div=this.parentElement;
+        div.style.display="none";
+    }
+}
+
+}
